10f8c22ab48b9a69d33e6ec6a6eefd6331649083
BahaEddinDridi <bahaeddine170@gmail.com>
2024-10-06 17:11:03 +0100

    Fixing the type error on accessToken

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth, { Session, User } from "next-auth";
+import NextAuth, { Session } from "next-auth";
 import { NextAuthOptions, JWT } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
